c04d86554b939d6c2d504becb2b57a46973030bc
Charles Ifoegbu <charlesifoegbu@gmail.com>
2022-07-04 16:17:57 +0100

    added the main_food_page along with some core dart codes(transition,dimensions,matrix)

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,79 @@
+import 'package:e_commerce/home/food_page_body.dart';
+import 'package:e_commerce/utils/colors.dart';
+import 'package:e_commerce/utils/dimensions.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // showing the header
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(
+                  top: Dimensions.height45, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 25),
+                        child: BigText(
+                          text: "Nigeria",
+                          color: AppColors.mainColor,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Port Harcourt",
+                            color: Colors.black,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: Dimensions.height45,
+                      height: Dimensions.height45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: Dimensions.iconSize24,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radius15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // showing the body
+          FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,42 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight / 2.64;
+  static double pageViewContainer = screenHeight / 3.84;
+  static double pageViewTextContainer = screenHeight / 7.03;
+
+//dynamic height; padding and margin
+  static double height1_2 = screenHeight / 703.33;
+  static double height10 = screenHeight / 84.4;
+  static double height15 = screenHeight / 56.27;
+  static double height20 = screenHeight / 42.2;
+  static double height30 = screenHeight / 28.13;
+  static double height45 = screenHeight / 18.76;
+
+//dynamic width; padding and margin
+  static double width10 = screenHeight / 84.4;
+  static double width15 = screenHeight / 56.27;
+  static double width20 = screenHeight / 42.2;
+  static double width30 = screenHeight / 28.13;
+
+//dynamic width; SizedBox and Container
+  static double mainwidth5 = screenWidth / 82.2;
+  static double mainwidth10 = screenWidth / 41.1;
+  static double mainwidth411 = screenWidth / 1.00;
+
+//font
+  static double font12 = screenHeight / 70.33;
+  static double font20 = screenHeight / 42.2;
+
+  //radius
+  static double radius15 = screenHeight / 56.27;
+  static double radius20 = screenHeight / 42.2;
+  static double radius30 = screenHeight / 28.13;
+
+  //icon size
+  static double iconSize15 = screenHeight / 56.27;
+  static double iconSize24 = screenHeight / 35.17;
+}
